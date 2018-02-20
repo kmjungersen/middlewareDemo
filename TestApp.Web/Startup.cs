3db7f8b24120e6d095736e4f2c85decc94dc6e45
Kurtis M Jungersen <kurtis.jungersen@asd-web.com>
2018-02-19 19:59:01 -0500

    modified 2 comments in startup.cs after writing README

--- TestApp.Web/Startup.cs
+++ TestApp.Web/Startup.cs
@@ -24,6 +24,7 @@ namespace TestApp.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // MVC service is registered here
             services.AddMvc();
 
             // Register our middleware for use as a service.  It's registered as a Singleton because
@@ -35,7 +36,7 @@ namespace TestApp.Web
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            // Inject our middleware component to the very beginning of the request pipeline.  This will
+            // Inject our middleware component at the very beginning of the request pipeline.  This will
             // get hit first when requests hit the server, and will get hit last as responses are on their
             // way back to the client.
             app.UseMyMiddleware();
